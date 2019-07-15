907f953df9d43092f65a579a5277c2291a0a39e5
Leo Kivikunnas <leo.kivikunnas@aalto.fi>
2019-07-15 20:26:18 +0300

    Add frontend

---
+++ build/precache-manifest.891ef68ab7f81cba1f00c682941d2c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93a88940d455bb89d8c8ee6126a9b0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c6f43f077105674ac2c",
+    "url": "/static/css/main.8e6daea6.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "1c6f43f077105674ac2c",
+    "url": "/static/js/main.4ad43056.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
